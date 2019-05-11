58aeab3606c2cf34b9f93e65fc4a7ad4253fd0da
neodimo <oensafi@hotmail.com>
2019-05-11 09:02:01 -0700

    destroyongameover

---
+++ PewPewPew/Assets/Scripts/DestroyOnGameOver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyOnGameOver : MonoBehaviour
+{
+    GameObject player;
+    Player closestPlayerClass;
+    string playerState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 2)
+        {
+            player = FindObjectOfType<Player>().gameObject;
+            closestPlayerClass = FindObjectOfType<Player>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerState = closestPlayerClass.ProcessState();
+        if (playerState == "Dead")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
