a3fd1ed6d18b9711c8322a790286e57a84c9f59a
neodimo <oensafi@hotmail.com>
2019-07-02 18:45:57 -0700

    portal particles

---
+++ piupiupiu/Assets/Scripts/VectorGridManipulate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorGridManipulate : MonoBehaviour
+{
+    VectorGridForce2 vgf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vgf = gameObject.GetComponent<VectorGridForce2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vgf.m_ForceScale = Mathf.PingPong(Time.deltaTime*3, .5f)-.1f;
+        //vgf.m_Radius
+    }
+}
